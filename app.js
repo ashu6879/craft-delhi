201e7ac86f26a5ff6a8937c3df082699c5bd0aab
ashutosh <144018820+ashu6879@users.noreply.github.com>
2025-06-15 21:45:45 +0530

    some changes

--- app.js
+++ app.js
@@ -3,17 +3,9 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-
-// ✅ Allow only your frontend domains
-const corsOptions = {
-  origin: ['http://localhost:3000', 'https://craftbuyer.onrender.com'],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // preflight handler
-
+app.use(cors());
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 
 module.exports = app;
