d01044950bda556fb654ae443eca2fa910b3bfad
ashutosh <144018820+ashu6879@users.noreply.github.com>
2025-07-27 23:11:04 +0530

    added role only to admin

--- controllers/adminDetailsController.js
+++ controllers/adminDetailsController.js
@@ -1,8 +1,14 @@
 const adminModel = require('../models/adminModel');
+require('dotenv').config();
 
 exports.getDashboardStats = (req, res) => {
-  adminModel.getDashboardStats((err, stats) => {
-    if (err) return res.status(500).json({ status: false, error: err });
-    res.json({ status: true, data: stats });
-  });
+  const {role_id} = req.user.role;
+  if(role_id == process.env.Admin_role_id){
+    adminModel.getDashboardStats((err, stats) => {
+      if (err) return res.status(500).json({ status: false, error: err });
+      res.json({ status: true, data: stats });
+    });
+  }else{
+    res.stats(401).json({ status: false, message: "Unauthorized Role" });
+  }
 };
