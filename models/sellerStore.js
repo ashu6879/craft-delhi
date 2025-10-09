385d61f94cc961c2fca2e02d2235133a4d605cf3
ashutosh <144018820+ashu6879@users.noreply.github.com>
2025-10-09 23:37:49 +0530

    seller slug added

--- models/sellerStore.js
+++ models/sellerStore.js
@@ -15,13 +15,15 @@ exports.getStoreBySellerId = (sellerId, callback) => {
       ss.description,
       ss.store_created_date,
       ss.business_number,
-      ss.store_image
+      ss.store_image,
+      ss.first_name,
+      ss.last_name
     FROM seller_stores ss
     WHERE seller_id = ? 
-    LIMIT 1 
+    LIMIT 1
   `;
 
-  db.query(query, [sellerId], async (err, results) => {
+  db.query(query, [sellerId], (err, results) => {
     if (err) return callback(err, null);
     if (results.length === 0) return callback(null, null);
 
@@ -29,26 +31,43 @@ exports.getStoreBySellerId = (sellerId, callback) => {
 
     // Generate slug if it doesn't exist
     if (!store.slug) {
-      let baseSlug = slugify(`${store.store_name}`, { lower: true });
+      let baseSlug = slugify(`${store.last_name}-${store.first_name}`, { lower: true });
       let slug = baseSlug;
       let counter = 1;
 
-      // Ensure uniqueness using async isSlugExists
-      while (await exports.isSlugExists(slug)) {
-        slug = `${baseSlug}-${counter}`;
-        counter++;
-      }
+      const checkSlugAndUpdate = () => {
+        db.query('SELECT id FROM seller_stores WHERE slug = ?', [slug], (err2, res2) => {
+          if (err2) return callback(err2, null);
 
-      const storeLink = `https://craftdelhi.com/store/${slug}`;
+          if (res2.length > 0) {
+            // Slug exists, add counter
+            slug = `${baseSlug}-${counter}`;
+            counter++;
+            checkSlugAndUpdate();
+          } else {
+            // Slug is unique, generate store link and save
+            const storeLink = `https://craftdelhi.com/store/${slug}`;
+            db.query(
+              'UPDATE seller_stores SET slug = ?, store_link = ? WHERE id = ?',
+              [slug, storeLink, store.id],
+              (err3) => {
+                if (err3) return callback(err3, null);
 
-      // Save slug and store_link in DB
-      await exports.updateSlug(store.id, slug, storeLink);
+                store.slug = slug;
+                store.store_link = storeLink;
 
-      store.slug = slug;
-      store.store_link = storeLink;
+                return callback(null, store);
+              }
+            );
+          }
+        });
+      };
+
+      checkSlugAndUpdate();
+    } else {
+      // Slug already exists
+      return callback(null, store);
     }
-
-    return callback(null, store);
   });
 };
 
@@ -97,21 +116,26 @@ exports.getStoreBySellerIdforAuth = (sellerId, callback) => {
   });
 };
 
-// ---------------- Async Utility Methods ---------------- //
-
 // Find store by ID
-exports.findById = async (id) => {
-  const [result] = await db.query('SELECT * FROM seller_stores WHERE id = ?', [id]);
-  return result; // assuming result is an array
+exports.findById = (id, callback) => {
+  db.query('SELECT * FROM seller_stores WHERE id = ? LIMIT 1', [id], (err, results) => {
+    if (err) return callback(err, null);
+    return callback(null, results[0] || null);
+  });
 };
 
 // Check if slug exists
-exports.isSlugExists = async (slug) => {
-  const [result] = await db.query('SELECT id FROM seller_stores WHERE slug = ?', [slug]);
-  return result.length > 0;
+exports.isSlugExists = (slug, callback) => {
+  db.query('SELECT id FROM seller_stores WHERE slug = ?', [slug], (err, results) => {
+    if (err) return callback(err, null);
+    return callback(null, results.length > 0);
+  });
 };
 
 // Update slug and store_link
-exports.updateSlug = async (id, slug, storeLink) => {
-  await db.query('UPDATE seller_stores SET slug = ?, store_link = ? WHERE id = ?', [slug, storeLink, id]);
+exports.updateSlug = (id, slug, storeLink, callback) => {
+  db.query('UPDATE seller_stores SET slug = ?, store_link = ? WHERE id = ?', [slug, storeLink, id], (err, results) => {
+    if (err) return callback(err, null);
+    return callback(null, results);
+  });
 };
