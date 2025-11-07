ac0356054a0d3ca4703abedb6057b71097e97f07
Ashutosh Sharma <ashup8998@gmail.com>
2025-11-07 16:39:37 +0530

    total users

--- models/adminModel.js
+++ models/adminModel.js
@@ -3,7 +3,7 @@ const db = require('../config/db'); // adjust path to your MySQL connection
 exports.getDashboardStats = (callback) => {
   const sql = `
     SELECT 
-    (SELECT COUNT(*) FROM users WHERE role IN (2, 3) AND user_status = 1) AS total_users,
+    (SELECT COUNT(*) FROM users WHERE role IN (2, 3)) AS total_users,
     (SELECT COUNT(*) FROM users WHERE role = 2 AND user_status = 1) AS active_sellers,
     (SELECT COUNT(*) FROM users WHERE role = 3 AND user_status = 1) AS active_buyers,
     (SELECT COUNT(*) FROM products WHERE admin_approval = 0) AS pending_products;
